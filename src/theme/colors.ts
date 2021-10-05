b12b5a79b9bea150f1a6b04594a750625673c662
Tiroras <shnyrov2000@gmail.com>
2021-10-05 13:47:59 +0400

    fix: colors name

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,7 +2,7 @@ export const colors = {
   white: '#ffffff',
   black: '#081F32',
   gray: ['#8E8E93', '#AEAEB2', '#C7C7CC', '#D1D1D6', '#E5E5EA', '#F2F2F7'],
-  indigo: '#5856D6',
-  adding: '#34C759',
-  descrtuctive: '#FF3B30',
+  purple: '#5856D6',
+  green: '#34C759',
+  red: '#FF3B30',
 }
