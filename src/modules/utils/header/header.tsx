e50788d1e24ac28f16e8b0df2e2c60e264e4aae7
Tiroras <shnyrov2000@gmail.com>
2021-10-05 17:51:32 +0400

    feat: add header for screens

--- src/modules/utils/header/header.tsx
+++ src/modules/utils/header/header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 import styled from 'styled-components/native'
 
 import { colors } from './../../../theme/colors'
