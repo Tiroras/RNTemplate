6ded76d32ece8b8d5a56d93116475d37bcc00fd7
Tiroras <shnyrov2000@gmail.com>
2021-10-07 14:05:24 +0400

    remove: garbage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
 import { NavigationContainer } from '@react-navigation/native'
 
 import { RootNavigation } from 'src/navigation/root'
 
-//import { client } from './graphql/client'
+import { client } from './graphql/client'
 import { AlertProvider } from './modules/alert-context'
-
-const client = new ApolloClient({
-  uri: 'https://rickandmortyapi.com/graphql',
-  cache: new InMemoryCache(),
-})
 
 export const App = () => {
   return (
